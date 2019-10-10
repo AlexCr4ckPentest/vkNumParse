1166d5fe49c1c9a9b26239b49a01a9db3a3c90df
AlexCr4ckPentest <48525682+AlexCr4ckPentest@users.noreply.github.com>
2019-10-10 23:23:43 +0300

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -2,5 +2,5 @@
 
 echo -e "\e[1;34m[*]\e[0m Installing some packages for fix errors...\n"
 apt install libxml2 libxslt libiconv -y
-echo -e "\e[1;32m[+]\e[0m Packages installed! Now start installing script..."
+echo -e "\e[1;32m[+]\e[0m Packages installed! Now start installing pip packages..."
 pip3 install -r requirements.txt
